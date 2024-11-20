b0e65fd53996a331aee56d729c5ab033ac629e17
nekihcom <kmmch.tech@gmail.com>
2024-11-20 23:29:02 +0900

    Reconsider UI Library

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <>
-            <footer className="w-full flex items-center justify-center py-3">
-                <small>&copy; 2024 Masaya Kemmochi</small>
-            </footer>
-        </>
-    );
-};
