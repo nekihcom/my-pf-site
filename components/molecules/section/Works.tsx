6ed4472142b680530b0f2e9b376270568aa38124
nekihcom <kmmch.tech@gmail.com>
2024-12-17 22:47:42 +0900

    WorkのURL間違いを修正

--- components/molecules/section/Works.tsx
+++ components/molecules/section/Works.tsx
@@ -13,8 +13,8 @@ const Works:FC = memo(() => {
       </Box>
 
       <Grid2 container spacing={{ xs:1, sm:5 }} columns={{ xs:1, md:2, lg:3 }} sx={{ justifyContent:'space-evenly' }}>
-        <WorkItem title={'Netflix Clone'} description={'Netflixのクローンアプリ、予告動画も見れます'} image={'/work/netflix-clone.png'} url={'https://todo-kappa-gilt.vercel.app/'} detailDescription={'ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。'} />
-        <WorkItem title={'Todoアプリ'} description={'Reactの練習で作成したTODOアプリ'} image={'/work/react-todo.png'} url={'https://nf-clone-sigma-amber.vercel.app/'} detailDescription={'ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。'} />
+        <WorkItem title={'Netflix Clone'} description={'Netflixのクローンアプリ、予告動画も見れます'} image={'/work/netflix-clone.png'} url={'https://todo-kappa-gilt.vercel.app/https://nf-clone-sigma-amber.vercel.app/'} detailDescription={'ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。'} />
+        <WorkItem title={'Todoアプリ'} description={'Reactの練習で作成したTODOアプリ'} image={'/work/react-todo.png'} url={'https://todo-kappa-gilt.vercel.app/'} detailDescription={'ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。'} />
         <WorkItem title={'StayHub'} description={'Javascriptの練習で作成したLP'} image={'/work/stayhub.png'} url={'https://superlative-seahorse-3e91fd.netlify.app/'} detailDescription={'ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。ここに詳細な説明が入ります。'} />
       </Grid2>
     </>
